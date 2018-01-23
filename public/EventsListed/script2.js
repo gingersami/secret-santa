179a0389573b59696ca1a75350cd85907e44e2dd
Chaim Finson <chaim0m@hotmail.com>
2018-01-23 16:35:04 +0200

    take6

--- public/EventsListed/script2.js
+++ public/EventsListed/script2.js
@@ -1,14 +1,25 @@
-function fetch() {
+
+var events=[]; 
+var anchor = $(".row")
+var getEventsFromServer = function () {
     $.ajax({
       method: "GET",
-      url: "/posts",
+      url: "/",
       success: function(data) {
-        posts = data;
-        _renderPosts();
-        // console.log(posts)
+        events = data;
+        _renderEvents()
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
       }
     });
   };
+
+  function _renderEvents() {
+    var source = $('#event-template').html();
+    var template = Handlebars.compile(source);
+    for (var i = 0; i < events.length; i++) {
+      var newHTML = template(events[i]);
+      anchor.append(newHTML);
+    }
+  }
